36c64f571471426ba4b43b63c53e028bd11b933f
中村駿輝 <shnklife@gmail.com>
2019-12-28 02:12:50 +0900

    仮置きのルーティングとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'captures#new'
+  get 'captures/execute' => 'captures#execute'
 end
